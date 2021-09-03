60a6e859df68a9204c11b3b855f39dc30af4022c
Jerry (Gerardo Sabetta) <gerardo.sabetta@attackiq.com>
2021-09-02 22:04:18 -0300

    rebasee todo askdjaskdj

---
+++ src/components/Vaccines.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ageRanges = ['birth', '1month', '2months', '3months', '6months'];
+const vaccines = ['hepatitis', 'sida', 'covid'];
+
+const Table = styled.div({
+  display: 'grid',
+  gridTemplateColumns: `repeat(${ageRanges.length + 1}, 1fr)`
+});
+
+const VaccineName = styled.div(({ vaccineName }: any) => ({
+  gridColumnStart: 1
+}));
+
+const Dose = styled.div(({ from, to, vaccineIndex }: any) => ({
+  gridColumnStart: from,
+  gridColumnEnd: to,
+  gridRowStart: vaccineIndex,
+  background: 'lightblue',
+  borderRadius: '5rem'
+}));
+
+const applications = [
+  {
+    vaccine: 'hepatitis',
+    dosis: [
+      { name: 'first', from: '1month', to: '2months' },
+      { name: 'second', from: '3months', to: '6months' }
+    ]
+  },
+  {
+    vaccine: 'covid',
+    dosis: [
+      { name: 'first', from: 'birth', to: '1month' },
+      { name: 'second', from: '3months', to: '3months' }
+    ]
+  }
+];
+
+const getVaccineRow = (vaccine: string): number =>
+  vaccines.findIndex((vac) => vac === vaccine);
+
+const getAgeColumn = (age: string): number =>
+  ageRanges.findIndex((ag) => ag === age);
+
+const Vaccines = () => {
+  return (
+    <Table>
+      <div>vaccine</div>
+
+      {ageRanges.map((ageRange) => (
+        <div>{ageRange}</div>
+      ))}
+
+      {vaccines.map((vaccine) => (
+        <VaccineName>{vaccine}</VaccineName>
+      ))}
+
+      {applications.map((application) => {
+        const vaccineIndex = getVaccineRow(application.vaccine) + 2; // +2 because findIndex is 0 indexed and we also dont want to overlap the header
+
+        return application.dosis.map((dose) => {
+          const from = getAgeColumn(dose.from) + 2; // +2 because findIndex is 0 indexed and we dont want to overlap the vaccine name
+          const to = getAgeColumn(dose.to) + 3; // +3 because all of the above and we want this to end at the right track of the grid cell
+
+          return (
+            <Dose vaccineIndex={vaccineIndex} from={from} to={to}>
+              {application.vaccine} {dose.name}
+            </Dose>
+          );
+        });
+      })}
+    </Table>
+  );
+};
+
+export default Vaccines;
